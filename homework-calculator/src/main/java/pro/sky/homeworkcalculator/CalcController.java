0b8393e8becfa47a6e058d3404446894183c3cc0
Евгений <ugend84@yandex.ru>
2022-04-28 16:08:38 +0300

    Изначально я делал и запрос и ответ типа Стринг, с  переводом в int и double. Менять ничего не стал, я так понял, не это главное, так и оставил

--- homework-calculator/src/main/java/pro/sky/homeworkcalculator/CalcController.java
+++ homework-calculator/src/main/java/pro/sky/homeworkcalculator/CalcController.java
@@ -18,11 +18,7 @@ public class CalcController {
 
     @GetMapping("/plus")
         public String answerSum (@RequestParam(value = "num1",required = false) String param1, @RequestParam(value = "num2",required = false) String param2) {
-        if (param1 == null || param2 == null) {
-            return "Ошибка!";
-        } else {
-            return calculateService.sum(param1, param2);
-        }
+         return calculateService.sum(param1, param2);
     }
 
 
@@ -36,8 +32,8 @@ public class CalcController {
     }
 
     @GetMapping("/multiply")
-    public String answerMult(@RequestParam(value = "num1",required = false) String param1, @RequestParam(value = "num2",required = false) String param2) {
-        if (param1 == null || param2 == null) {
+    public String answerMult(@RequestParam(name = "num1",required = false) String param1, @RequestParam(name = "num2",required = false) String param2) {
+        if (null==param1 || null==param2) {
             return "Ошибка!";
         } else {
             return calculateService.multy(param1, param2);
@@ -46,9 +42,6 @@ public class CalcController {
 
     @GetMapping("/divide")
     public String answerDiv(@RequestParam(value = "num1",required = false) String param1, @RequestParam(value = "num2",required = false) String param2) {
-        if (param2.equals("0")) {
-            return "Деление на 0";
-        }
         if (param1 == null || param2 == null) {
             return "Ошибка!";
         } else {
